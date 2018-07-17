9fcd19bdddbf7aa82a505dc06378bc91daa8cd00
Mathew Zaleski <matz@zaleski.ca>
2018-07-17 12:37:15 -0400

    comment cruft properly (sigh)

--- src/components/__tests__/GiphyApp_spec.tsx
+++ src/components/__tests__/GiphyApp_spec.tsx
@@ -1,21 +1,14 @@
 import * as React from 'react'
-//import * as TestUtils from 'react-dom/test-utils'
+// import * as TestUtils from 'react-dom/test-utils'
 import * as ReactShallowRenderer from 'react-test-renderer/shallow'
 
-//import { createStore } from 'redux'
-//import { reducers } from '../../reducers'
+// import { createStore } from 'redux'
+// import { reducers } from '../../reducers'
 
 import { GiphyApp } from '../GiphyApp';
 
 describe('<GiphyApp />', () => {
   it('renders', () => {
-    //const store = createStore(reducers)
-//    const Rearenderer = new ReactShallowRenderer()
-//      <GiphyApp  store={store} />
-
-//const renderer: ReactShallowRenderer.ShallowRenderer = ReactShallowRenderer.createRenderer
-//    const ReactShallowRenderer.ShallowRender renderer = new ReactShallowRenderer()
-
     expect(ReactShallowRenderer.createRenderer().render(
       <GiphyApp />
     )).toMatchSnapshot()
