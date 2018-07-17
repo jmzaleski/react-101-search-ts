79a4baf9c7fde5ee84a2f5d44805e5896ac8ed35
Mathew Zaleski <matz@zaleski.ca>
2018-07-17 12:43:57 -0400

    comment out cruft

--- src/components/__tests__/GiphyApp_spec.tsx
+++ src/components/__tests__/GiphyApp_spec.tsx
@@ -1,16 +1,11 @@
 import * as React from 'react'
-// import * as TestUtils from 'react-dom/test-utils'
 import * as ReactShallowRenderer from 'react-test-renderer/shallow'
-
-// import { createStore } from 'redux'
-// import { reducers } from '../../reducers'
-
 import { GiphyApp } from '../GiphyApp';
 
 describe('<GiphyApp />', () => {
   it('renders', () => {
     expect(ReactShallowRenderer.createRenderer().render(
       <GiphyApp />
-    )).toMatchSnapshot()
+    )).toMatchSnapshot() // jest writes a file into the file system that subsequently compares against
   })
 });
