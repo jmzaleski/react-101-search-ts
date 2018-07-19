8a3bebd97b41fbde96d3fcd22fdf53ccca29eb10
Mathew Zaleski <matz@zaleski.ca>
2018-07-19 11:21:04 -0400

    should not export argument click hander type sig
    That's just for the function passed to the div

--- src/components/GifItem.tsx
+++ src/components/GifItem.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 
 import * as giphy from 'restyped-giphy-api';
 
+// GifItem should pass giphy.GIFObject to handler when it is clicked
 export type tOnGifSelectCallback = (gifobj: giphy.GIFObject) => any;
-export type tOnClickCallback = () => any;
 
-// The GifItem component takes two props from its parent
-// 1. GIFObject as returned from the giphy search endpoint
-// 2. a function to call back when the gifitem is clicked
+// The GifItem component takes two (key?) props from its parent
+// 1. giphy.GIFObject as returned from the giphy search endpoint
+// 2. a function to call back, passing gifobj, when the gifitem is clicked
 // 3. key. I'm not sure this is useful
 // TODO: what about that key prop? it's just floating around in GifItem. stuff it in alt.
 
@@ -18,7 +18,9 @@ export interface IGifItemProps {
     };
 
 export function GifItem(props: IGifItemProps) {
-    // avoid lambda's in JSX or tslint will reject
+    type tOnClickCallback = () => any; //button click handler gets no parms
+
+    // avoid lambda's in JSX or tslint will reject, warning about performance issues
     const onGifSelectCallbackClosure: tOnClickCallback = () => props.onGifSelectCallback(props.gifobj)
     return (
         <div className="gif-item" onClick={onGifSelectCallbackClosure}>
