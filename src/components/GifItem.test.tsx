f2562aa655ef12e9dcabde50f6b562b7e5180d17
Mathew Zaleski <matz@zaleski.ca>
2018-07-19 13:45:39 -0400

    factor stuff in to before

--- src/components/GifItem.test.tsx
+++ src/components/GifItem.test.tsx
@@ -3,58 +3,58 @@ import { GifItem, tOnGifSelectCallback } from './GifItem';
 import * as enzyme from 'enzyme';
 import * as giphy from 'restyped-giphy-api';
 import * as Adapter from 'enzyme-adapter-react-16';
+import * as ReactShallowRenderer from 'react-test-renderer/shallow'
 
-// our mock of a giphy GIFObject
-var mockGiphyGifObject: giphy.GIFObject;
+// mock of a giphy.GIFObject
 const MockedConstructor = jest.fn<giphy.GIFObject>(() => {
     return { images: { downsized: { url: 'mockurl' } } }
 });
 
+// JSX translated GifItem
+var ele: any;
+var mockGiphyGifObject: giphy.GIFObject;
+var mockedCallbackForGifItem: jest.Mock<tOnGifSelectCallback>;
+
 beforeEach(() => {
     mockGiphyGifObject = MockedConstructor();
-    // console.log("mockgiphyobject:");
-    // console.log(mockGiphyGifObject);
+    mockedCallbackForGifItem = jest.fn((gifobj:giphy.GIFObject) => null);
+    ele = <GifItem gifobj={mockGiphyGifObject} onGifSelectCallback={mockedCallbackForGifItem} key={"foo"}/> ;
 });
     
-import * as ReactShallowRenderer from 'react-test-renderer/shallow'
 
 // shallow render to snapshot file
 // turns out the file is useful to snoop at to figure out selector (see next test)
 describe('<GiphItem />', () => {
     const cb: tOnGifSelectCallback = (gifobj:giphy.GIFObject) => null;
     it('renders', () => {
-      expect(ReactShallowRenderer.createRenderer().render(
-        <GifItem gifobj={mockGiphyGifObject} onGifSelectCallback={cb} key={"foo"}/>          
-      )).toMatchSnapshot()
+      expect(ReactShallowRenderer.createRenderer().render( ele )).toMatchSnapshot();
     })
   });
 
 // shallow render GifItem and search in resulting virtual DOM to make sure 
 // that mock giphy image prop url has made it to URL of actual image node
-
-describe('simulate click', () => {
-    it('shallow render GifItem ', () => {
-        var clickCount: number = 0;
-        var passedParm: giphy.GIFObject | null = null;
-        const callbackForMock: tOnGifSelectCallback = (gifobj:giphy.GIFObject) => { 
-            clickCount = clickCount+1; 
-            passedParm = gifobj;
-            return null
-        };
+describe('check IMG node', () => {
+    it('IMG src = mockurl ', () => {
         enzyme.configure({ adapter: new Adapter() });
-        const gifitem = enzyme.shallow(
-            <GifItem gifobj={mockGiphyGifObject} onGifSelectCallback={callbackForMock} key={"foo"}/>
-            );
+        const gifitem = enzyme.shallow(ele);
         // query strings for find are called "selectors". There are lots of flavours.
         // see: https://github.com/airbnb/enzyme/blob/master/docs/api/selector.md
         expect(gifitem.contains(<img src="mockurl"/> )).toEqual(true);
-
-        // test that simulated click bumps clickCount. Zero before.. 1 after..
-        expect(clickCount).toEqual(0);
-        gifitem.find('[className="gif-item"]').simulate("click");
-        expect(clickCount).toEqual(1); // less crufty ways to do this using jest fancy stuff
-
-        // make sure that the mock GIFObject was passed to the callback
-        expect(passedParm).toEqual(mockGiphyGifObject);
     })
 });
+
+describe('GifItem calls click handler correctly', () => {
+    it('simulate a click on shallow rendering of GifItem', () => {
+        enzyme.configure({ adapter: new Adapter() });
+        const gifitem = enzyme.shallow( ele );
+        // handler has NOT  been called before simulate click 
+        expect(mockedCallbackForGifItem.mock.calls.length).toEqual(0);
+
+        gifitem.find('[className="gif-item"]').simulate("click"); 
+
+        // The first argument of the first call to the handler should be mocked GIFObject
+        expect(mockedCallbackForGifItem.mock.calls.length).toEqual(1);
+        expect(mockedCallbackForGifItem.mock.calls[0][0]).toBe(mockGiphyGifObject); 
+    })
+});
+
